bbe52889e7c825209eecb12f32e5a3c5e13f9a83
AsamFernando <ferasam88@gmail.com>
2020-02-08 22:10:07 -0300

    resuelto segunda clase

--- python-primeros-pasos/operaNoConm.py
+++ python-primeros-pasos/operaNoConm.py
@@ -4,5 +4,45 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+var1=3
+lock= threading.Lock()
 
+def dormir():
+    time.sleep(3)
+
+def sumarUno():
+    global var1
+    global lock
+    try:
+        lock.acquire()
+        var1+=1
+    finally:
+        lock.release()
+def multiplicarPorDos():
+    global var1
+    var1*=2
+    try:
+        lock.acquire()
+        var1*=2
+    finally:
+        lock.release()
+
+
+logging.info('creando los threads')
+t1=threading.Thread(target=sumarUno)
+t2=threading.Thread(target=multiplicarPorDos)
+
+
+#dormir()
+
+t1.start()
+
+dormir()
+t2.start()
+
+
+t1.join()
+t2.join()
+
+logging.info(f'var1 es: {var1}')
 
